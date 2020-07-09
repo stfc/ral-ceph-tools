0ac2b4aaf502cb2713f0fe193db3d72d4740195a
Foster <james.foster@stfc.ac.uk>
2020-07-09 11:25:39 +0100

    Updating osdsearch and ticket-drive with comments and final exception handling changes.

--- disk-tooling/osdsearch.sh
+++ disk-tooling/osdsearch.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+#Checks if host can be connected to
 host=$2
+if ! timeout 10 ssh $host exit; then
+    echo "Error: SSH connection failed"
+    exit 1
+fi
+#Multipath identification
+#multipath -ll will fail if used on a non-multipath host, so the command is run conditionally
+#isMultipath variable is set to true or false depending on if the command succeeded or not
 multipath_check() {
 if timeout 10 ssh $1 multipath -ll ; then
     isMultipath=true
@@ -8,26 +16,67 @@ else
     isMultipath=false
 fi
 }
-
+#multipath -ll output is sent to /dev/null to prevent it from displaying in console window
+#The command output isn't relevant, just the command's success or failure
 multipath_check $host &> /dev/null
 
 path=""
 parsedpath=""
 
+echo "Searching for OSD..."
 if [[ "$isMultipath" == true ]]; then
-
-    for i in $(ssh $host "ls /dev/mapper/*" | grep -v [0-9] | grep -v control ); do if ( ssh $host smartctl -a -d scsi $i | grep $1 ); then path="$i"; break; fi; done
-
-    vg=$(ssh $host pvs | grep $path | awk '{print $2}' )
-    osd=$(ssh $host ls -l /var/lib/ceph/osd/ceph-*/block 2>/dev/null | grep "$vg" 2>/dev/null | cut -d'/' -f6 | cut -d'-' -f2)
-    parsedpath=$(ssh $host echo "$path" | cut -d '/' -f4)
+#If the host is multipath, search for /dev/mapper
+    for i in $(timeout 10 ssh $host "ls /dev/mapper/*" | grep -v [0-9] | grep -v control ); do if (timeout 10 ssh $host smartctl -a -d scsi $i | grep $1 ); then path="$i"; break; fi; done
+#Use the path to find the VG
+    vg=$(timeout 10 ssh $host pvs | grep $path | awk '{print $2}' )
+    if [ -z "$vg" ]; then
+        echo "Error: Volume Group not found"
+        exit 1
+    fi
+#Use the VG to search for the OSD number
+    osd=$(timeout 10 ssh $host ls -l /var/lib/ceph/osd/ceph-*/block 2>/dev/null | grep "$vg" 2>/dev/null | cut -d'/' -f6 | cut -d'-' -f2)
+#OSD sanitization
+    if [ -z "$osd" ]; then
+        echo "Error: OSD not found"
+        exit 1
+    fi
+#Parsing the pathname to only have the last part, i.e mpatha
+    parsedpath=$(timeout 10 ssh $host echo "$path" | cut -d '/' -f4)
+    if [ -z "$parsedpath" ]; then
+        echo "Error: Could not parse path correctly"
+        exit 1
+    fi
 
 else
-    for i in $(ssh $host "ls /dev/sd*" | grep -v [0-9] ); do if ( ssh $host smartctl -a $i | grep $1 ); then path="$i"; break; fi; done
-
-    osd=$(ssh $host df | grep "${path}1"| cut -d'-' -f2)
-    parsedpath=$(ssh $host echo "${path%1}" | cut -d '/' -f3)
+#If the host is not multipath, search for /dev/sd
+#The pathname can then be used to search directly for the OSD
+    for i in $(timeout 10 ssh $host "ls /dev/sd*" | grep -v [0-9] ); do if (timeout 10 ssh $host smartctl -a $i | grep $1 ); then path="$i"; break; fi; done
+    osd=$(timeout 10 ssh $host df | grep "${path}1"| cut -d'-' -f2)
+#If this fails, the OSD is a bluestore
+    if [ -z "$osd" ]; then
+#Search for the VG using the pathname instead, then use the VG to find the OSD
+        vg=$(timeout 10 ssh $host pvs | grep $path | awk '{print $2}' )
+        if [ -z "$vg" ]; then
+            echo "Error: Volume Group not found"
+            exit 1
+        fi
+        osd=$(timeout 10 ssh $host ls -l /var/lib/ceph/osd/ceph-*/block 2>/dev/null | grep "$vg" 2>/dev/null | cut -d'/' -f6 | cut -d'-' -f2)
+        if [ -z "$osd" ]; then
+            echo "Error: OSD not found"
+            exit 1
+        fi
+    fi
+    if ! [[ "$osd" =~ ^[0-9]+$ ]]; then
+        echo "Error: OSD not found"
+        exit 1
+    fi
+#Parsing the pathname to only have the last part, i.e sdaa
+    parsedpath=$(timeout 10 ssh $host echo "${path%1}" | cut -d '/' -f3)
+    if [ -z "$parsedpath" ]; then
+        echo "Error: Path parsing failed (OSD not found)"
+        exit 1
+    fi
 fi
 
-echo "$parsedpath"
-echo "$osd"
+echo "${parsedpath} is the path"
+echo "${osd} is the OSD number"
--- disk-tooling/ticket-drive.sh
+++ disk-tooling/ticket-drive.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+#Checks if the host can be connected to
 host=$2
+if ! timeout 10 ssh $host exit; then
+    echo "Error: SSH connection failed"
+    exit 1
+fi
+#Uses osdsearch to find the osd and parsed pathname
+source ~/jamesf/osdsearch $1 $2
 echo "Creating ticket..."
-source ~/jamesf/osdsearch $1 $2 &> /dev/null
+#Uses report_osd_alltypes to print a report of the osd
 source ~/jamesf/report_osd_alltypes $osd > /tmp/osdreport
 echo "" >> /tmp/osdreport
+#Uses check_osd to verify if osd is ready to be removed
 ~/tom/check_osd.sh $2 $parsedpath >> /tmp/osdreport
 
+#Creates the ticket in specific format
+echo ""
 echo "Status: new" > /tmp/driveticket
 echo "Subject: Disk Replacement $1 on $2" >> /tmp/driveticket
 echo "" >> /tmp/driveticket
@@ -15,16 +25,19 @@ echo "Path: $parsedpath" >> /tmp/driveticket
 echo "$(cat /tmp/osdreport)" >> /tmp/driveticket
 cat /tmp/driveticket
 
+#Submits ticket via sendmail
 read -p "Submit this ticket? (Y/N) " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    if ! sendmail ceph-disk@helpdesk.gridpp.rl.ac.uk < /tmp/driveticket
+    if ! timeout 10 sendmail ceph-disk@helpdesk.gridpp.rl.ac.uk < /tmp/driveticket
     then
         echo -e "\nTicket submission failed. (Ceph-Disk)"
+        exit
     fi
-    if ! sendmail hardware@helpdesk.gridpp.rl.ac.uk < /tmp/driveticket
+    if ! timeout 10 sendmail hardware@helpdesk.gridpp.rl.ac.uk < /tmp/driveticket
     then
         echo -e "\nTicket submission failed. (Fabric-Hardware)"
+        exit
     fi
     echo -e "\nTicket submitted to Ceph-Disk and Fabric-Hardware successfully!"
 else

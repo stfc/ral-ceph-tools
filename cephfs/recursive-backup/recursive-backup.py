023742d3b2c7fff4d2d79e297e4190832ee264ec
Tom Byrne <tom.byrne@stfc.ac.uk>
2021-01-08 11:15:50 +0000

    improve recursive backup scripts logging and reporting
       * add json summary line when running the rsyncs
       * add timestamps to the info and debug lines

--- cephfs/recursive-backup/recursive-backup.py
+++ cephfs/recursive-backup/recursive-backup.py
@@ -1,10 +1,58 @@
 #!/usr/bin/python3
 import os
 import sys
+import json
 import time
 import shlex
 import argparse
 import subprocess
+from datetime import datetime, timedelta
+
+warnings = False
+
+debug = False
+info = False
+
+def timestamp_print( message ):
+    print ("# {} {}".format(time.strftime('[%Y-%m-%d %H:%M:%S]'), message))
+
+def debug_print( message ):
+    if (debug):
+        timestamp_print("DEBUG> " + message)
+
+def info_print( message ):
+    if (info or debug):
+        timestamp_print("INFO> " + message)
+
+def warn_print( message ):
+    global warnings
+    warnings = True
+    timestamp_print("WARN> " + message)
+
+def exit_print ( success, message ):
+    if success:
+        timestamp_print("FINISHED SUCCESS> " + message)
+    else:
+        timestamp_print("FINISHED FAILED> " + message)
+
+def print_summary_json ( success, message ):
+    body = {
+        "type": "backup_summary",
+        "success": success,
+        "start_time": start_time,
+        "exit_time": time.time(),
+        "message": message
+    }
+    print(json.dumps(body))
+
+def log_and_exit ( success, message ):
+    if (args.run):
+        print_summary_json( success, message)
+    exit_print( success, message );
+    rc = 1
+    if success and not warnings:
+        rc = 0
+    sys.exit(rc)
 
 start_time=time.time()
 
@@ -21,7 +69,9 @@ parser = argparse.ArgumentParser(description="Generate rsync commands to back up
 
 parser.add_argument("src", help="source CephFS directory to backup")
 parser.add_argument("dst", help="destination directory to store the backup")
-parser.add_argument("time", help="epoch time of last backup (set to 0 for full sync)", type=int)
+parser.add_argument("-t", "--time", help="epoch time of last backup", type=int)
+parser.add_argument("-d", "--days", help="days since last backup. Use instead of --time", type=int)
+parser.add_argument("--full", help="do a full backup", action="store_true")
 parser.add_argument("-f", "--maxfiles", help="maximum number of files per rsync. defaults to 100000", type=int)
 parser.add_argument("-b", "--maxbytes", help="maximum bytes per rsync. defaults to 100GB", type=int)
 parser.add_argument("-s", "--safety", help="number of seconds before last backup time to still consider the directory changed. defaults to 3600 (1h)", type=int, default=3600)
@@ -32,15 +82,28 @@ parser.add_argument('-v', '--verbose', help="one '-v' for informational messages
 
 args = parser.parse_args()
 
-time = args.time - args.safety
+if not (args.time or args.days or args.full):
+    log_and_exit( False, "backup interval not specified, use --time, --days or --full")
 
-debug = False
-info = False
+if (args.time and args.days) or (args.time and args.full) or (args.days and args.full):
+    log_and_exit( False, "specify ONLY one of --time, --days or --full as a backup interval")
+
+if args.days:
+    backup_time_obj = datetime.now() - timedelta(days=args.days)
+    backup_time_wo_safety = backup_time_obj.timestamp()
+    backup_time = backup_time_wo_safety - args.safety
+elif args.full:
+    backup_time = 0
+else:
+    backup_time = args.time - args.safety
+
+
 if (args.verbose >= 2):
     debug = True
 elif (args.verbose == 1):
     info = True
 
+info_print("backup time is {}".format(backup_time))
 
 if (args.maxfiles != None):
     max_files = args.maxfiles
@@ -50,15 +113,6 @@ if (args.maxbytes != None):
 
 
 rsync_cmd_list = []
-
-def debug_print( message ):
-    if (debug):
-        print("# DEBUG> " + message)
-
-def info_print( message ):
-    if (info or debug):
-        print("# INFO> " + message)
-
 
 def rsync_full( directory ):
     generate_rsync( directory, False)
@@ -88,6 +142,7 @@ def get_rctime( directory ):
         rctime = rctime_sp.stdout.split(b'.')[0]
         return {"out": int(rctime), "stderr": rctime_sp.stderr, "rc": rctime_sp.returncode}
     else:
+        warn_print("error while getting rctime of: {}".format(directory))
         return {"out": 0, "stderr": rctime_sp.stderr, "rc": rctime_sp.returncode}
 
 def get_rfiles( directory ):
@@ -95,6 +150,7 @@ def get_rfiles( directory ):
     if (rfiles_sp.returncode == 0):
         return {"out": int(rfiles_sp.stdout), "stderr": rfiles_sp.stderr, "rc": rfiles_sp.returncode}
     else:
+        warn_print("error while getting rfiles of: {}".format(directory))
         return {"out": 0, "stderr": rfiles_sp.stderr, "rc": rfiles_sp.returncode}
 
 def get_rbytes( directory ):
@@ -102,6 +158,7 @@ def get_rbytes( directory ):
     if (rbytes_sp.returncode == 0):
         return {"out": int(rbytes_sp.stdout), "stderr": rbytes_sp.stderr, "rc": rbytes_sp.returncode}
     else:
+        warn_print("error while getting rbytes of: {}".format(directory))
         return {"out": 0, "stderr": rbytes_sp.stderr, "rc": rbytes_sp.returncode}
 
 def get_rsubdirs( directory ):
@@ -109,12 +166,13 @@ def get_rsubdirs( directory ):
     if (rsubdirs_sp.returncode == 0):
         return {"out": int(rsubdirs_sp.stdout), "stderr": rsubdirs_sp.stderr, "rc": rsubdirs_sp.returncode}
     else:
+        warn_print("error while getting rsubdirs of: {}".format(directory))
         return {"out": 0, "stderr": rsubdirs_sp.stderr, "rc": rsubdirs_sp.returncode}
 
 def recurse_rsync( directory ):
     debug_print("starting recurse rsync for directory {}".format(directory))
     rctime = get_rctime( directory )
-    if (rctime["rc"] == 0 and rctime["out"] > time):
+    if (rctime["rc"] == 0 and rctime["out"] > backup_time):
         info_print( "directory {} has a newer rctime of {}, backing up".format(directory, rctime["out"]) )
         rsubdirs = get_rsubdirs( directory )
         rfiles = get_rfiles( directory )
@@ -135,32 +193,27 @@ def recurse_rsync( directory ):
                     if not os.path.islink( os.path.join(directory,subdir)):
                         recurse_rsync( os.path.join(directory,subdir))
                 break
-    else:
+    elif (rctime["rc"] == 0):  
         debug_print( "directory {} has an older rctime of {}, skipping".format(directory, rctime["out"]) )
+    else:
+        warn_print( "nonzero exit code while getting rctime for {}, not backing up".format(directory) )
 
 
 if (args.checksrc):
     srcmnt = os.path.ismount(args.src)
     if not srcmnt:
-        print("source directory {} is not a mountpoint and check specified".format(args.src))
-        print("backup exited with errors")
-        sys.exit(1)
+        log_and_exit(success=False, message="Source directory {} is not a mountpoint (and check specified)".format(args.src))
 
 if (args.checkdst):
     dstmnt = os.path.ismount(args.dst)
     if not dstmnt:
-        print("dest directory {} is not a mountpoint and check specified".format(args.dst))
-        print("backup exited with errors")
-        sys.exit(1)
-
+        log_and_exit(success=False, message="Destination directory {} is not a mountpoint (and check specified)".format(args.dst))
 
 debug_print( "changing to source directory {}".format(args.src) )
 os.chdir( args.src )
 test_rctime = get_rctime( "." )
 if (test_rctime["rc"] != 0):
-    print("source directory {} did not return a CephFS rctime, so probably is not a CephFS mount".format(args.src))
-    print("backup exited with errors")
-    sys.exit(1)
+    log_and_exit(success=False, message="Source directory {} did not return a CephFS rctime, so probably is not a CephFS mount and cannot be backed up".format(args.src))
 else:
     debug_print("src dir returned a cephFS rctime, starting ")
 
@@ -168,17 +221,19 @@ recurse_rsync( "." )
 
 if (args.run):
     cmd_list_len = len(rsync_cmd_list)
-    print("{} rsync commands to run".format(cmd_list_len))
+    timestamp_print("{} rsync commands to run".format(cmd_list_len))
     for (i, rsync_cmd) in enumerate(rsync_cmd_list):
-        print("{}/{} running '{}'".format(i+1, cmd_list_len, " ".join(rsync_cmd)))
+        timestamp_print("{}/{} running '{}'".format(i+1, cmd_list_len, " ".join(rsync_cmd)))
         rsync = run_rsync(rsync_cmd)
         if (rsync["rc"] == 0):
-            print("Success")
+            timestamp_print("Success")
             print(rsync["stdout"].decode('ascii'))
         else:
-            print("failure (return code: {})".format(rsync["rc"]))
+            timestamp_print("failure (return code: {})".format(rsync["rc"]))
             print(rsync["stdout"].decode('ascii'))
             print(rsync["stderr"].decode('ascii'))
-            print("backup exited with errors")
-            sys.exit(1)
-    print("SUCCESS: backup started at {} finished successfully".format(start_time))
+            warn_print("rsync failed, continuing")
+    if warnings is True:
+       log_and_exit(success=False, message="backup started at {} ({}) finished with warnings".format(time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(start_time)), start_time))
+    else:
+       log_and_exit(success=True, message="backup started at {} ({}) finished successfully".format( time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(start_time)), start_time))

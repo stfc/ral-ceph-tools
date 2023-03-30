1f984d47a80dc146d0039538f65d0f2391354364
Jo-STFC <jyothish.thomas@stfc.ac.uk>
2023-03-30 11:38:18 +0000

    monitor and aquilon utility scripts from Jyothish

--- utilities/logsearch.sh
+++ utilities/logsearch.sh
@@ -1,4 +1,4 @@
-hosts=( "ceph-gw4.gridpp.rl.ac.uk" "ceph-gw5.gridpp.rl.ac.uk" "ceph-gw6.gridpp.rl.ac.uk" "ceph-gw7.gridpp.rl.ac.uk" "ceph-gw10.gridpp.rl.ac.uk" "ceph-gw11.gridpp.rl.ac.uk" "ceph-gw15.gridpp.rl.ac.uk" "ceph-gw16.gridpp.rl.ac.uk" "ceph-svc01.gridpp.rl.ac.uk" "ceph-svc02.gridpp.rl.ac.uk" "ceph-svc03.gridpp.rl.ac.uk" "ceph-svc05.gridpp.rl.ac.uk" "ceph-svc97.gridpp.rl.ac.uk" "ceph-svc98.gridpp.rl.ac.uk" "ceph-svc99.gridpp.rl.ac.uk" );
+hosts=`cat hostlist.txt`
 #searches xrootd and gridftp logs and ip address gw hosts listed above for parameter passed to script
 #example: bash logsearch.sh 31.147.202.178 searches for that ip address in logs and ssh
 #
